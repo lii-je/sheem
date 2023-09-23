90950d7c733d7a8b63e2f5c54cd26dd290a1e196
leejaek <leejh9049@gmail.com>
2023-09-23 11:31:08 +0900

    [add] initial

---
+++ src/test/java/com/lije/sheem/SheemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lije.sheem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
